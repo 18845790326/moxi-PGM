97d079d2160db84fb281ed31d04ac94f749e79c5
zhb <1078654443@qq.com>
2022-09-23 15:53:43 +0800

    3

---
+++ cloud-api-payment/src/main/java/com/buba/springcloud/PayMentMain.java
@@ -0,0 +1,13 @@
+package com.buba.springcloud;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class PayMentMain {
+    public static void main(String[] args) {
+        SpringApplication.run(PayMentMain.class,args);
+    }
+}
